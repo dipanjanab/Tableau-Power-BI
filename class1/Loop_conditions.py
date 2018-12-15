bfd3cc733cb4a6b0b2abc740f04b0ee08fa02196
dipanjanab <dipanjanab09@gmail.com>
2018-12-15 16:30:38 +0530

    Code dict

---
+++ class1/Loop_conditions.py
@@ -0,0 +1,20 @@
+#question 11
+
+n = int{input("Enter n :  ")}
+m = int{input("Enter m :  ")}
+
+result_list = {}
+temp = []
+
+for i in range (0,n):
+    temp.clear()
+    for j in range (0,m):
+            temp.append(i*j)
+            print(i*j)
+        result_list.append(tmp)
+
+    print(result_list)
+
+
+
+

b81d9e6f42d907b23081d26e45a7f3858243c9a4
Dipanjana Bhattacharyya <26251133+dipanjanab@users.noreply.github.com>
2024-11-25 23:48:03 +0530

    Delete class1 directory

--- class1/palindrome.py
+++
@@ -1,7 +0,0 @@
-originalstring= input ("please input a string")
-
-reverse = originalstring[-1::-1]
-if reverse == originalstring:
-   print("It's a palindrome")
-else:
-   print("It's not a palindrome")
